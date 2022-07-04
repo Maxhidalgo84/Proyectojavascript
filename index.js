935af234f00c77c0066485b41dec92caa6ee71ec
Maximo Hidalgo <maxhidalgo84@gmail.com>
2022-07-04 17:47:07 -0300

    user y seleccion js

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
+// Pedido de usuario y luego sigue eleccion de bicicleta (sin vincularlo con el DOM)
+
+//LOGIN
+
+ let usuario = prompt("ingrese su usuario")
+    
+    
+        while(usuario == "") {
+            alert("usuario incorrecto")
+        usuario = prompt("Por favor ingrese un usuario");}
+        
+        let mensaje2 = (`Hola ${usuario} `)
+        alert(mensaje2)
+
+        let contraseña = prompt("ingrese contraseña")
+
+            while (contraseña != "coder") {
+            alert ("Contraseña incorrecta");
+
+            contraseña = prompt("ingrese coder") 
+
+            }
+        alert(`${usuario} se logueo exitosamente`); 
+    
+
+//Eleccion de la bici
+
+        function ingresar(x){   
+            if (x==1){
+            return "100000"
+            
+            }  
+                
+            else if (x==2){
+            return "80000"
+            }
+    
+            else if (x==3){
+                return "50000"
+                }
+    
+            else if (x==4) {
+            return "40000"
+                }    
+            
+            else { 
+                return "0"
+            }  
+    }
+       
+    let valor = parseInt(prompt("ingrese cual bici desea comprar (1, 2, 3 o 4)"));
+    
+    while (valor!=1 && valor!=2 && valor!=3 && valor!=4){
+        alert("ingreso invalido");
+        valor = parseInt(prompt("ingrese cual bici desea comprar (1, 2, 3 o 4)"));}
+    
+    precio = parseInt(ingresar(valor));
+    
+    const suma = (a,b) => a +b;
+    const resta = (a,b) => a - b;
+    const iva = x => x *0.21;
+    
+    let precioProducto = precio;
+    let descuento = precioProducto*0.10;
+    let nuevoPrecio = resta(suma(precioProducto,iva(precioProducto)),descuento); 
+     
+    alert(`Usted eligio el modelo de bicicleta Mountain Bike ${valor} y el valor con descuento es de $${nuevoPrecio}`);
+    
+
+
+//// Pedido de usuario y luego sigue eleccion de bicicleta (vicunlado con el DOM)
+  
+//LOGIN
+/* 
+
 const getValueInput = () => {
 
     let usuario = document.getElementById("uid").value;
@@ -23,9 +98,10 @@ const getValueInput = () => {
         }
 }
 
-
+//ELECCION DE BICI
 
 const seleccion = (x) =>{
+  
     let precioss;
     let modelo;
    
@@ -69,21 +145,4 @@ let mensaje = (`Usted eligio el modelo de bicicleta ${modelo} y el valor con des
 document.getElementById("demo2").innerHTML = mensaje;
 
 }
- 
-
-
-   // let modelo= prompt("ingrese el modelo de bicicleta").toUpperCase();
-    
-/* //alert(modelo);
-let precio = parseInt(getValueInput(precioss))
-
-const suma = (a,b) => a +b;
-const resta = (a,b) => a - b;
-const iva = x => x *0.21;
-
-let precioProducto = precio;
-let descuento = precioProducto*0.10;
-
-let nuevoPrecio = resta(suma(precioProducto,iva(precioProducto)),descuento); 
-
-alert(`Usted eligio el modelo de bicicleta ${modelo} y el valor con descuento es ${nuevoPrecio}`);    */
+  */
