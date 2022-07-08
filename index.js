cfb130667bd03f2cb6b2136bd472783374c5b989
Maximo Hidalgo <maxhidalgo84@gmail.com>
2022-07-08 11:07:29 -0300

    arrays

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Pedido de usuario y luego sigue eleccion de bicicleta (sin vincularlo con el DOM)
 
 //LOGIN
-
+/* 
  let usuario = prompt("ingrese su usuario")
     
     
@@ -21,128 +21,54 @@
 
             }
         alert(`${usuario} se logueo exitosamente`); 
-    
+     */
 
 //Eleccion de la bici
 
-        function ingresar(x){   
-            if (x==1){
-            return "100000"
-            
-            }  
-                
-            else if (x==2){
-            return "80000"
-            }
-    
-            else if (x==3){
-                return "50000"
-                }
-    
-            else if (x==4) {
-            return "40000"
-                }    
-            
-            else { 
-                return "0"
-            }  
-    }
+
+class Producto {
+    constructor(id,nombre,precio){
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.vendido = false;
+}
+
+vender() {
+    this.vendido = true
+}
+}
+
+
+let productos = [];
+
+productos.push(new Producto(1,"MB 1", 100000 ));
+productos.push(new Producto(2,"MB 2", 80000 ));
+productos.push(new Producto(3,"MB 3", 50000 ));
+productos.push(new Producto(4,"MB 4", 40000 ));
        
-    let valor = parseInt(prompt("ingrese cual bici desea comprar (1, 2, 3 o 4)"));
+    let valor = 2
     
-    while (valor!=1 && valor!=2 && valor!=3 && valor!=4){
+  /*   while (valor!=1 && valor!=2 && valor!=3 && valor!=4){
         alert("ingreso invalido");
-        valor = parseInt(prompt("ingrese cual bici desea comprar (1, 2, 3 o 4)"));}
+        valor = parseInt(prompt("ingrese cual bici desea comprar (1, 2, 3 o 4)"));} */
     
-    precio = parseInt(ingresar(valor));
-    
+        
+
+for (const producto of productos) {
+       if (producto.id == (valor)){   
+    precioBici = (producto.precio)
+    producto.vender()} 
+} 
+
+      
     const suma = (a,b) => a +b;
     const resta = (a,b) => a - b;
     const iva = x => x *0.21;
     
-    let precioProducto = precio;
+    let precioProducto = precioBici;
     let descuento = precioProducto*0.10;
     let nuevoPrecio = resta(suma(precioProducto,iva(precioProducto)),descuento); 
      
-    alert(`Usted eligio el modelo de bicicleta Mountain Bike ${valor} y el valor con descuento es de $${nuevoPrecio}`);
+    console.log(`Usted eligio el modelo de bicicleta Mountain Bike ${valor} y el valor con descuento es de $${nuevoPrecio}`);
     
-
-
-//// Pedido de usuario y luego sigue eleccion de bicicleta (vicunlado con el DOM)
-  
-//LOGIN
-/* 
-
-const getValueInput = () => {
-
-    let usuario = document.getElementById("uid").value;
-
-    if (usuario == "") {
-        alert("usuario requerido");}
-        else {
-
-        let mensaje2 = (`Hola ${usuario}`)
-        document.getElementById("demo3").innerHTML = mensaje2;
-        let contraseña = document.getElementById("pass").value;
-
-            while (contraseña != "coder") {
-            alert ("Contraseña incorrecta");
-
-            contraseña = prompt("ingrese coder") 
-
-            }
-         
-
-        alert(`bienvenido ${usuario}`); 
-    
-        }
-}
-
-//ELECCION DE BICI
-
-const seleccion = (x) =>{
-  
-    let precioss;
-    let modelo;
-   
-
-    switch(x){
-    case "1":
-        precioss = "100000";
-        modelo = "MB1";
-        break;
-        
-    case "2":
-        precioss = "80000";
-        modelo = "MB2";
-        break;
-        
-    case "3":
-        precioss = "30000";
-        modelo = "MB3";
-        break;
-
-    default:
-        return ("no se encontro el modelo")
-        
-    }  
-
-    
-    
-let precio = parseInt(precioss)
-
-const suma = (a,b) => a +b;
-const resta = (a,b) => a - b;
-const iva = x => x *0.21;
-
-let precioProducto = precio;
-let descuento = precioProducto*0.10;
-
-let nuevoPrecio = resta(suma(precioProducto,iva(precioProducto)),descuento); 
-
-let mensaje = (`Usted eligio el modelo de bicicleta ${modelo} y el valor con descuento es de $${nuevoPrecio}`);   
-
-document.getElementById("demo2").innerHTML = mensaje;
-
-}
-  */
