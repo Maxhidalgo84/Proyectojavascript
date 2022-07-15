4bd0ca026cf6a7c624af6a43625ff34cb65c2204
Maximo Hidalgo <maxhidalgo84@gmail.com>
2022-07-15 13:13:54 -0300

    eleccion por id

--- index.js
+++ index.js
@@ -22,6 +22,31 @@
             }
         alert(`${usuario} se logueo exitosamente`); 
      */
+       let padre = document.getElementById("demo3") 
+        let error1 = document.getElementById("error1")      
+        let usuario = document.getElementById("name")    
+        let formulario = document.getElementById("formulario")
+        let boton01 = document.getElementById("envia")
+
+  
+           if(formulario){ formulario.addEventListener("submit", (e)=> {
+            e.preventDefault()
+            if (e.target.correo.value.includes("@") && e.target.correo.value.includes(".")){
+                let mensaje = document.createElement("div")
+                mensaje.className= "demo2";
+                mensaje.innerText = `Bienvenido ${usuario.value}`
+                padre.append(mensaje)
+            }else {
+                alert("correo invalido")
+                let mensaje = document.createElement("div")
+                mensaje.innerHTML = "no es un correo"
+                error1.appendChild(mensaje)
+                e.correo.value = "";
+            }
+        
+        })}    
+
+
 
 //Eleccion de la bici
 
@@ -66,63 +91,71 @@ contenedor.innerHTML = ` <div class="card">
                             <h5 class="card-title">${producto.nombre} </h5>
                             <p class="card-text"> ${producto.descripcion} </p>
                             <h5 class="card-title"> $${producto.precio}</h5>
-                            <a href="#" id="${producto.id}" onclick="seleccion(this.id)"  class="btn btn-primary">Comprar</a>
+                            <a href="#" id="${producto.id}"  class="btn btn-primary">Comprar</a>
                             </div>
                         </div>`
+if(padre2){                    
 padre2.appendChild(contenedor)
+
+const bicicleta =  document.getElementById(`${producto.id}`)
+bicicleta.onclick = () => {eleccion(producto.id)}
 }
+}
+
 
 let valor;
 
 let carrito=[];
 
-const seleccion = (obj) => {
-valor = obj
-let borrar = document.getElementById('total')
-while( borrar.hasChildNodes()) {
-    borrar.removeChild(borrar.firstChild)
-} 
 
-for (const producto of productos) {
-    if (producto.id == (valor)){   
-        carrito.push(new Carrito(producto.nombre,producto.precio))
-       verPrecio = producto.precio
+//const seleccion = (obj) => {
+    function eleccion(x) {
+        valor = x
+        let verPrecio
+        let borrar = document.getElementById('total')
+        while (borrar.hasChildNodes()) {
+            borrar.removeChild(borrar.firstChild)
+        }
+
+        for (const producto of productos) {
+            if (producto.id == (valor)) {
+                carrito.push(new Carrito(producto.nombre, producto.precio))
+                verPrecio = producto.precio
+            }
+        }
+
+        /* for (const items of carrito) {
+            if (carrito.id == (valor)) {
+                carrito.splice
+            
+            }
+        
+        } */
+        alert(`Usted eligio el modelo de bicicleta Mountain Bike ${valor} y el valor es de $${verPrecio}`)
+
+
+        let detalle = carrito.reduce((acumulaNombre, elemento) => acumulaNombre + "\n " + elemento.nombre + ": $" + elemento.precio, "")
+        let total = carrito.reduce((acumulador, elemento) => acumulador + elemento.precio, 0)
+
+        const Compras = detalle.concat("\n TOTAL: $" + total)
+
+        alert(Compras)
+        console.log(Compras)
+
+        let padre3 = document.getElementById("total")
+
+        let totales = document.createElement("div")
+
+        totales.innerHTML = `${Compras}`
+        totales.id = "clear"
+        padre3.appendChild(totales)
+
     }
-} 
-
-/* for (const items of carrito) {
-    if (carrito.id == (valor)) {
-        carrito.splice
-    
-    }
-
-} */
-
-alert(`Usted eligio el modelo de bicicleta Mountain Bike ${valor} y el valor es de $${verPrecio}`);
 
 
-let detalle = carrito.reduce((acumulaNombre,elemento) => acumulaNombre + "\n " + elemento.nombre + ": $" + elemento.precio,"")
-let total = carrito.reduce((acumulador,elemento) => acumulador + elemento.precio,0)
+/*
 
-const Compras = detalle.concat("\n TOTAL: $"+total)
-
-alert(Compras)
-console.log(Compras); 
-
-let padre3 = document.getElementById("total")
-
-let totales = document.createElement("div")
-
-totales.innerHTML = `${Compras}`;
-totales.id = "clear";
-padre3.appendChild(totales)
-
-
-}
-
-
-
-/* for (const item of carrito) {
+for (const item of carrito) {
     let li = document.createElement("li")
     li.innerHTML = `<p>Producto: ${item.nombre}  <b>$${item.precio}</b></p>`;
     padre3.append(li);
@@ -132,5 +165,5 @@ let totales = document.createElement("div")
 totales.innerHTML=`<b>Total: $${total}</b>`
 padre3.appendChild(totales); 
 
-}
-  */
+}*/
+  
